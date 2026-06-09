0b0a54cfb631dbc6b51bb3331d637c8b79a86814
Rohit111888 <konatam.12@gmail.com>
2026-06-09 16:39:55 -0400

    Completed FastAPI Expense Tracker with PostgreSQL

---
+++ app/database.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# Load environment variables
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Create PostgreSQL engine
+engine = create_engine(DATABASE_URL)
+
+# Create session
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+# Base class for models
+Base = declarative_base()
